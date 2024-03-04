be8317738849c5173a3c0b67dce04b858cdf4599
Roel de Cort <roel@decort.tech>
2024-03-04 01:27:32 +0100

    add nvim

---
+++ dot_config/nvim/lua/custom/mappings.lua
@@ -0,0 +1,28 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+    --  format with conform
+    ["<leader>fm"] = {
+      function()
+        require("conform").format()
+      end,
+      "formatting",
+    }
+
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
+  },
+}
+
+M.dctec = {
+   n = {
+     ["<leader>pf"] = {"builtin.find_files", "Telescope Search"}
+ },
+}
+
+return M
