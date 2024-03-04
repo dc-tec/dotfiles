e0daa9017995a02f92aab557c6e15be240e3d36e
Roel de Cort <roel@decort.tech>
2024-03-04 23:16:06 +0100

    nvim config changes

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+local opt = vim.opt
+
+-- pandoc
+opt.spell = false -- disable spelling
+opt.foldmethod = "manual" -- fold when I want to
+opt.foldenable = false -- never fold!
+
+-- scrolling
+opt.scrolloff = 8 -- start scrolling 8 lines from the edge
+opt.linebreak = true -- wrap lines at convenient points
+
+vim.g.mkdp_browser = "/mnt/c/Program Files (x86)/Microsoft/Edge/Application/msedge.exe" -- Set Markdown Preview to use Edge
---
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        bashls = {},
+        gopls = {},
+        bicep = {},
+        yamlls = {},
+        marksman = {},
+        powershell_es = {},
+        azure_pipelines_ls = {},
+        terraformls = {},
+        helm_ls = {},
+      },
+    },
+  },
+}
